a21117670a5af3061ab7356794ff9fa9d430e534
Kevin Johr <kevin.johr@posteo.de>
2022-09-18 21:50:47 +0200

    implement store step one

--- src/components/CalendarDay.vue
+++ src/components/CalendarDay.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card">
     <div class="card-header text-center" role="button">
-      <strong>Montag</strong>
+      <strong>{{day.fullName}}</strong>
     </div>
     <div class="card-body">
       <CalendarEvent />
@@ -17,6 +17,28 @@ export default {
   components: {
     CalendarEvent,
   },
+  // Array-Schreibweise; Nicht zu empfehlen
+  // props: ["day"],
+
+  // Objekt-Schreibweise
+  props: {
+    day: {
+      type: Object,
+      required: true,
+      default: function() {
+        return {
+          id: -1,
+          fullName: "Fehler",
+          events: [],
+        };
+      },
+      validator: function(value) {
+        if (Object.keys(value).includes("id")) {
+          return true;
+        }
+      },
+    },
+  }
 };
 </script>
 
